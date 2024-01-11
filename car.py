175aa2370d9eab423b4acc3c817b01248a957496
c038644 <106696284+c038644@users.noreply.github.com>
2024-01-11 10:23:40 +0000

    Update car.py

--- car.py
+++ car.py
@@ -112,6 +112,8 @@ with st.spinner('Updating Report...'):
 
     g1, g2, g3 = st.columns((1,1,1))
 
+    st.write(ten_most_important_df)
+
     fig = px.bar(ten_most_important_df, x = 'Feature', y='Importance')
     
     fig.update_layout(title_text="Local Features Graph",title_x=0,margin= dict(l=0,r=10,b=10,t=30), yaxis_title=None, xaxis_title=None)
