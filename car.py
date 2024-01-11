688ce6b03baccb35a6cdf8e58e54c98ba3714232
c038644 <106696284+c038644@users.noreply.github.com>
2024-01-11 10:15:34 +0000

    Update car.py

--- car.py
+++ car.py
@@ -7,130 +7,20 @@ import plotly.graph_objects as go
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import GradientBoostingClassifier
 
-def local():
-
-  #Local Features Case for a chosen Selected Customer
-  test_df = pd.read_csv("files/P7_test_df.csv")
-
-  Selected_Customer = pd.read_csv("files/selection.csv")
-
-  Selected_Customer = Selected_Customer.drop(columns=['Unnamed: 0'])
-  test_df = test_df.drop(columns=['Unnamed: 0'])
-
-  feature_list = list(test_df.columns)
-
-  X = test_df.drop(['TARGET'], axis=1).values
-  y = test_df['TARGET'].values
-
-  data = Selected_Customer.drop(['TARGET'], axis=1).values
-
-  X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
-
-  undersample = RandomUnderSampler(sampling_strategy=1)
-
-  X_train, y_train = undersample.fit_resample(X_train, y_train)
-
-  rfc = RandomForestClassifier(max_depth=13, min_samples_leaf=2, min_samples_split=8, n_estimators=552)
-  
-  score = rfc.fit(X_train, y_train).predict(data)
-
-  Credit_given_test = np.max(rfc.predict_proba(data))
-
-  if score==0:
-    credit_score=Credit_given_test
-
-  else:
-    credit_score=(1-Credit_given_test)
-
-   # Get numerical feature importances
-  importances = list(rfc.feature_importances_)
-
-  # List of tuples with variable and importance
-  feature_importances = [(feature, round(importance, 2)) for feature, importance in zip(feature_list, importances)]
-
-  # Sort the feature importances by most important first
-  feature_importances = sorted(feature_importances, key = lambda x: x[1], reverse = True)
-
-  #Ten most important features
-  ten_most_important = feature_importances[0:10]
-
-  ten_most_important_df = pd.DataFrame(ten_most_important)
-
-  ten_most_important_df.columns = ['Feature', 'Importance']
-
-  ten_most_important_df['Credit Score'] = credit_score
-
-  ten_most_important_df['Credit Granted?'] = None
-
-  if credit_score>=0.35:
-    ten_most_important_df['Credit Granted?'] = ten_most_important_df['Credit Granted?'].fillna('Yes')
-  elif credit_score>=0.25:
-    ten_most_important_df['Credit Granted?'] = ten_most_important_df['Credit Granted?'].fillna('Risky')
-  else:
-    ten_most_important_df['Credit Granted?'] = ten_most_important_df['Credit Granted?'].fillna('No')
-
-  ten_most_important_df.to_csv("files/Customer_score.csv")
-
-  print('Customer Score Ready')
-  return(ten_most_important_df)
-
-
-def global_data():
-
-  test_df = pd.read_csv("files/P7_test_df.csv")
-
-  #Global Features Case
-
-  feature_list = list(test_df.columns)
-
-  X = test_df.drop(['TARGET'], axis=1).values
-  y = test_df['TARGET'].values
-
-  X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
-
-  undersample = RandomUnderSampler(sampling_strategy=1)
-
-  X_train, y_train = undersample.fit_resample(X_train, y_train)
-
-  rfc = RandomForestClassifier(max_depth=13, min_samples_leaf=2, min_samples_split=8, n_estimators=552)
-
-  rfc.fit(X_train, y_train)
-
-  # Get numerical feature importances
-  importances = list(rfc.feature_importances_)
-
-  # List of tuples with variable and importance
-  feature_importances = [(feature, round(importance, 2)) for feature, importance in zip(feature_list, importances)]
-
-  # Sort the feature importances by most important first
-  feature_importances = sorted(feature_importances, key = lambda x: x[1], reverse = True)
-
-  #Ten most important features
-  ten_most_important = feature_importances[0:10]
-
-  Global_Features = pd.DataFrame(ten_most_important)
-
-  Global_Features.columns = ['Feature', 'Importance']
-
-  Global_Features.to_csv("files/Global_Features.csv")
-
-  # Print out the feature and importances 
-  return(Global_Features)
-
 #NUMBER_KFOLDS = 2 
 #NUMBER_ITER = 1
 #NUMBER_REPEATS = 1
 RANDOM_STATE = 42
 GBC_METRIC = 'squared_error'
 
-st.set_page_config(page_title='Credit Rating Calculator',  layout='wide', page_icon=':Calculator:')
+st.set_page_config(page_title='Car Insurance Fraud Calculator',  layout='wide', page_icon=':Calculator:')
 
 #this is the header
  
 t1, t2 = st.columns((0.07,1)) 
 
-t2.title("Credit Rating Calculator")
-t2.markdown("with Global and Local Customer Data")
+t2.title("Car Insurance Fraud Calculator")
+t2.markdown("Powered with Machine Learning")
 
 
 with st.spinner('Updating Report...'):
@@ -146,19 +36,20 @@ with st.spinner('Updating Report...'):
 
     data_df['fraud_reported'] = data_df['fraud_reported'].astype(int)
 
+    cleaned_df = pd.get_dummies(data_df)
+
     Customer = st.selectbox('Select Customer', Customer_ID, help = 'Filter report to show only one customer')
 
     if Customer:
-        Selected_Customer = data_df.loc[data_df['policy_number'] == Customer]
+        Selected_Customer = cleaned_df.loc[cleaned_df['policy_number'] == Customer]
         st.write(Selected_Customer)
         #Selected_Customer.to_csv("files/selection.csv")
         #local = requests.get("local").json()
         #st.json(local) 
     
     #local()
-    Selected_Customer_cleaned_df = pd.get_dummies(Selected_Customer)
+       
     
-    cleaned_df = pd.get_dummies(data_df)
     feature_list = list(cleaned_df.columns)
 
     X = cleaned_df.drop(['fraud_reported'], axis=1).values
